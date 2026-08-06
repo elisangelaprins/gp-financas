d20fed16131a809d900726d9b63ac583b146ccc2
Elisangela Prins <elisangela.prinz@gmail.com>
2026-08-06 10:16:46 -0300

    fix: altera jest.config.ts para jest.config.js para compatibilidade nativa no CI

---
+++ backend/jest.config.js
@@ -0,0 +1,14 @@
+/** @type {import('jest').Config} */
+const config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { useESM: true }],
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+};
+
+export default config;
